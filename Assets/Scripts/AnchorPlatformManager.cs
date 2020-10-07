bf22a1c891fc76e316a6e0acb22e00a77d04b1d0
Giedrius <266934@via.dk>
2020-10-07 22:23:19 +0200

    Readded proj files

---
+++ Assets/Scripts/AnchorPlatformManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using TMPro;
+
+public class AnchorPlatformManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject anchorPlatformObj;
+
+    [SerializeField]
+    private TextMeshProUGUI feedbackText;
+
+    private List<DetectedPlane> detectedPlanes = new List<DetectedPlane>();
+
+    private bool platformSpawned;
+
+    private void Awake()
+    {
+        feedbackText.text = "Scanning ... ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Session.GetTrackables<DetectedPlane>(detectedPlanes, TrackableQueryFilter.New);
+
+        if(detectedPlanes.Count == 1)
+        {
+            feedbackText.text = "Place the platform";
+        }
+
+
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.Default;
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                if(Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+                {
+                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                    if(!platformSpawned)
+                    {
+                        Instantiate(anchorPlatformObj, anchor.transform.position, anchor.transform.rotation, anchor.transform);
+                        feedbackText.text = "";
+                        platformSpawned = true;
+                    }                   
+                }
+            }
+        }
+    }
+}
